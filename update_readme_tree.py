5652e8c6ebb819518ccc7af51674fb5d33017a24
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-02-04 11:50:25 +0100

    Fix update_readme_tree.py

--- update_readme_tree.py
+++ update_readme_tree.py
@@ -21,7 +21,7 @@ def get_tree(base_dir: Path, linkify: bool, is_root: bool) -> str:
     """Get the tree of the project directories and subdirectories as a string."""
     subdirs = [dir_ for dir_ in base_dir.iterdir() if dir_.is_dir()]
     level = len(base_dir.parts) - 1
-    indent = "│    " * (level)
+    indent = "\t" * (level)
     tree = f"{dir_path_to_str(base_dir, linkify)}<br>\n" if is_root else ""
     for subdir in subdirs:
         sep = "└─── " if subdir == subdirs[-1] else "├─── "
