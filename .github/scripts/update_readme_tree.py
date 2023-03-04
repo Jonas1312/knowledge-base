76384274dcd0740c9b7e04091d3d02b86554b24a
Jonas Maison <Jonas1312@users.noreply.github.com>
2023-03-04 17:30:33 +0100

    Update tree script

--- .github/scripts/update_readme_tree.py
+++ .github/scripts/update_readme_tree.py
@@ -44,10 +44,10 @@ def get_tree(base_dir: Path, linkify: bool, is_root: bool) -> str:
     level = len(base_dir.parts) - 1
     indent = ""
     if level > 0:
-        indent = "├─" * level
+        indent = "├───" * level
     tree = f"{dir_path_to_str(base_dir, linkify)}<br>\n" if is_root else ""
     for subdir in subdirs:
-        sep = "└─ " if subdir == subdirs[-1] else "├─ "
+        sep = "└─── " if subdir == subdirs[-1] else "├─── "
         tree += f"{indent}{sep}{dir_path_to_str(subdir, linkify)}<br>\n"
         tree += get_tree(subdir, linkify=linkify, is_root=False)
     return tree
