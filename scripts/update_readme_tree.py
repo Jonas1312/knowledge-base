03af9197a03a15b09dc60f1baca396e5f81e456c
Jonas Maison <Jonas1312@users.noreply.github.com>
2025-02-01 10:41:38 +0100

    Update notes

--- scripts/update_readme_tree.py
+++ scripts/update_readme_tree.py
@@ -78,7 +78,7 @@ class TreeGenerator:
 
     def _add_directory(self, directory, index, last_index, prefix, connector):
         self.tree.append(
-            f'{prefix.replace(" ", HTML_SPACE)}{connector.replace(" ", HTML_SPACE)} {dir_path_to_str(directory, linkify=True)}<br>'
+            f"{prefix.replace(' ', HTML_SPACE)}{connector.replace(' ', HTML_SPACE)} {dir_path_to_str(directory, linkify=True)}<br>"
         )
         if index != last_index:
             prefix += PIPE_PREFIX
